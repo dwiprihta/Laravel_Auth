ee064fbaf409f13ed2e6db647c1940ea507cb0fe
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-06-30 20:04:46 +0700

    ada style register btn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class=" btn btn-primary" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
